497f559725773a42415cad39990ebe29b4ad9a2e
caioagralemos <caioagralemos@live.com>
2023-05-31 22:35:15 -0300

    4 - Objetos

--- 4 - Objects/types.ts
+++ 4 - Objects/types.ts
@@ -10,3 +10,41 @@ console.log(coordenada)
 function doublePointTwo(point: Point): Point {
     return {x: point.x * 2, y: point.y * 2}
 }
+
+type User = {
+    readonly id: number; // dar a propriedade readonly num atributo de um type faz com que,
+    // ele seja imutavel
+    username: string;
+}
+
+var usuario: User = {
+    id: 1,
+    username: 'caioagralemos'
+}
+
+// usuario.id = 2 // Não é possível atribuir a 'id' porque é uma propriedade de somente leitura
+
+type Circle = {
+    radius: number
+}
+
+type Color = {
+    color: string
+}
+
+type colorCircle = Circle & Color; // Tipos de Interseção
+
+const happyFace: colorCircle = {
+    radius: 4,
+    color: 'yellow'
+}
+
+type colorCircles = Circle & Color & {
+    amount: number // Tipos de interseção com novas propriedades exclusivas
+}
+
+var seteCirculosVermelhos: colorCircles = {
+    radius: 5,
+    color: 'red',
+    amount: 7
+}
